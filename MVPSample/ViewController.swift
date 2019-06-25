8592200ea370df58170a4e477c9fc69201dadd8a
masaibar <masaibar.dev@gmail.com>
2019-06-25 13:51:50 +0900

    Initial Commit

---
+++ MVPSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVPSample
+//
+//  Created by Nishihara Ryo on 2019/06/25.
+//  Copyright © 2019 Nishihara Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
